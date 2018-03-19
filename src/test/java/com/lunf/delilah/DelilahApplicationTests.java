885e1d3784e1871a2c3274ed164052ff6873f56d
Cuong Nguyen <nhatcuong.nguyen@gmail.com>
2018-03-19 22:19:00 +0700

    First commit

---
+++ src/test/java/com/lunf/delilah/DelilahApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lunf.delilah;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DelilahApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
